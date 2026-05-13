845474fb8d59be8c48f566c0bf06b84e8abba555
DanielLab <danillp@gmail.com>
2026-05-13 08:54:33 -0600

    Se integran los sensores

--- Lab_Tk_aleatorios/src/main.cpp
+++ Lab_Tk_aleatorios/src/main.cpp
@@ -1,26 +1,55 @@
-#include <Arduino.h>
-
-// Variables globales
-int n = 0; // Contador consecutivo
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+#define BMP280_ADDRESS 0x76
+Adafruit_BMP280 bmp;  // use I2C interface
+#include <DHT.h>  
+// Set up the DHT sensor 
+DHT dht(4, DHT22);
+float temp ;
+float  hum;
+float pres ;
+float alt;
+int n;
 
 void setup() {
-    // Iniciamos la comunicación serial a 115200 baudios
-    Serial.begin(115200);
-    delay(1000); // Pequeña pausa para estabilizar la conexión
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hola, ESP32!");  
+  while (!Serial) delay(100);  // wait for native usb
+  Serial.println(F("BMP280 test"));
 
-    // Opcional: Imprimir el encabezado CSV
-    Serial.println("N,temp,pres,hum,alt");
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                     "try a different address!"));
+    while (1) delay(10);  // Stop code execution if the sensor is not found.
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
-    // Generación de valores aleatorios (flotantes)
-    // random(min, max) devuelve un entero, lo dividimos para obtener decimales
-    float temp = random(1500, 3500) / 100.0;    // Rango: 15.00 - 35.00
-    float pres = random(95000, 105000) / 100.0; // Rango: 950.00 - 1050.00
-    float hum  = random(3000, 9000) / 100.0;    // Rango: 30.00 - 90.00
-    float alt  = random(0, 500000) / 100.0;     // Rango: 0.00 - 5000.00
+  // put your main code here, to run repeatedly:
 
-    // Impresión en formato CSV
+
+  // Comprobamos si ha habido algún error en la lectura inicial o global
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Error obteniendo los datos del sensor DHT22");
+    return;
+  }
+
+  temp  = dht.readTemperature();
+  hum    = dht.readHumidity();
+  pres = bmp.readPressure();
+  alt = bmp.readAltitude(presion);
+
     Serial.print(n);
     Serial.print(",");
     Serial.print(temp, 2); // 2 decimales
@@ -29,11 +58,9 @@ void loop() {
     Serial.print(",");
     Serial.print(hum, 2);
     Serial.print(",");
-    Serial.println(alt, 2); // println al final para el salto de línea
+    Serial.println(alt, 2);
 
-    // Incrementar contador
-    n++;
-
-    // Esperar 1 segundo entre lecturas
-    delay(1000);
+  delay(1000); // this speeds up the simulation
+  n++;
 }
+
